7bd3dac6f00cc5b4d0cf30e89a63c53a7de2628a
mattallenn <104325813+mattallenn@users.noreply.github.com>
2023-08-15 17:47:28 -0400

    updated init.lua

--- init.lua
+++ init.lua
@@ -1,5 +1,8 @@
-require("mattallen.set")
-require("mattallen.remap")
+# Change the path to the location of VimScode.lua
+# For example: require("mattallen/VimsCode").open_in_vscode()
+# Usage in nvim :VimsCode
+# To change the command name, change the name of the function (VimScode)
 
-vim.cmd([[command! -nargs=0 VimScode :lua require("mattallen.VimScode").open_in_vscode()]])
+vim.cmd([[command! -nargs=0 VimScode :lua require("pathto/VimsCode").open_in_vscode()]])
+
 
